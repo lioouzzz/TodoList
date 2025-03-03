26b90583a4d4628af9cefbe3c2259e565d0aa4fb
lioouzzz <lioou.work@gmail.com>
2025-03-03 22:34:15 +0800

    chore:first commit

---
+++ Models/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
